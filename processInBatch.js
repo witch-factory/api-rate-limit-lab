8c8f27c740bb4469113289dcdd8e8e8f093af6db
witch-factory <soakdma37@gmail.com>
2025-01-31 22:29:33 +0900

    배치로 나눠서 요청하는 로직 구현

---
+++ processInBatch.js
@@ -0,0 +1,12 @@
+const processInBatch = async (tasks, batchSize) => {
+  const results = [];
+
+  for (let i = 0; i < tasks.length; i += batchSize) {
+    const batch = tasks.slice(i, i + batchSize);
+    const batchResults = await Promise.all(batch.map((task) => task()));
+    results.push(...batchResults);
+  }
+  return results;
+};
+
+module.exports = processInBatch;
--- test.js
+++ test.js
@@ -1,23 +1,28 @@
 const http = require("http");
+const processInBatch = require("./processInBatch");
 
 const SERVER_URL = "http://localhost:3000";
-const NUM_REQUESTS = 6; // 동시에 보낼 요청 개수 지정
+const NUM_REQUESTS = 4; // 동시에 보낼 요청 개수 지정
+const BATCH_SIZE = 5; // 한 번에 보낼 요청 개수 지정
 
 const sendRequest = (index) => {
   return new Promise((resolve, reject) => {
     console.log(`요청 ${index} 시작`);
 
     const req = http.get(SERVER_URL, (res) => {
+      let result;
+
       res.on("data", (chunk) => {
         const data = JSON.parse(chunk);
         console.log(
           `요청 ${index} 메시지: ${data.message}, 현재 서버에 남은 요청 개수: ${data.requestNumber}`
         );
+        result = data;
       });
 
       res.on("end", () => {
         console.log(`요청 ${index} 성공`);
-        resolve();
+        resolve(result);
       });
     });
 
@@ -37,7 +42,9 @@ const testRequests = async () => {
     promises.push(() => sendRequest(i));
   }
 
-  await Promise.allSettled(promises.map((fn) => fn()));
+  const results = await processInBatch(promises, BATCH_SIZE);
+  console.log(`모든 요청 완료: ${results.length}개`);
+  console.log(results);
 };
 
 testRequests();
